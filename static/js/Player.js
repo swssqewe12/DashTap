47fb3d73cda4c2748304137444d6e41b875c0474
swssqewe12 <david@callanan.ie>
2017-12-08 18:41:53 +0000

    Added fake jump

--- static/js/Player.js
+++ static/js/Player.js
@@ -5,19 +5,42 @@ function Player()
 	this.side = new Aim(0);
 	this.sideX = 200;
 	this.size = 80;
+	this.altitude = 0;
+	this.velocity = 0;
+	this.gravity = -0.4;
+	// between 4 and 7
+	setInterval(() => this.jump(7), 1500);
 	//window.addEventListener("mousedown", this.mouseDown.bind(this))
 	//window.addEventListener("mouseup", this.mouseUp.bind(this))
 }
 
+Player.prototype.jump = function(power)
+{
+	this.velocity += power;
+}
+
+Player.prototype.updatePhysics = function()
+{
+	this.altitude += this.velocity;
+	this.velocity += this.gravity;
+
+	if (this.altitude < 0)
+	{
+		this.altitude = 0;
+		this.velocity = 0;
+	}
+}
+
 Player.prototype.update = function()
 {
-	this.side.achieveFrac(0.25);
+	this.updatePhysics();
+	this.side.achieveFrac(0.3);
 	this.x = this.side.value + this.sideX - this.size / 2;
 }
 
 Player.prototype.draw = function()
 {
-	ctx.altitude = 0;
+	ctx.altitude = this.altitude * 0.25;
 	ctx.fill("red");
 	ctx.rect(this.x, this.y, this.size, this.size);
 }
--- static/js/game.js
+++ static/js/game.js
@@ -22,24 +22,20 @@ Game.prototype.reset = function()
 	window.onresize();
 
 	this.player = new Player();
-
 	this.isRunning = true;
 }
 
 Game.prototype.update = function()
 {
 	this.player.update();
-	ctx.setAltitudeRelativePoint(this.player.x + this.player.size / 2, this.player.y);
+	ctx.setAltitudeRelativePoint(this.player.x + this.player.size / 2, this.player.y + this.player.size / 2);
 }
-
-test = 0;
 
 Game.prototype.draw = function()
 {
 	ctx.clear();
 	this.player.draw();
-	ctx.altitude = test;
-	test -= 0.1;
+	ctx.altitude = -this.player.altitude * 0.7;
 	ctx.fill("brown");
-	ctx.rect(0, 400, 400, 50);
+	ctx.rect(0, 450, 400, 50);
 }
